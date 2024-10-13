2fe21fce3407ada42bf1d154acbf105476700fc0
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-13 12:00:53 +0530

    solved the navbar issue

--- components/Navbar.js
+++ components/Navbar.js
@@ -45,8 +45,7 @@ const Navbar = () => {
                     </ul>
 
                     <div className='lg:hidden flex  flex-col items-end gap-y-5'>
-
-                        <button className='' onClick={() => { toggleNavbar() }}>
+                        <button onClick={() => { toggleNavbar() }}>
                             {isClick ? (
                                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0,0,256,256">
                                     <g fill="#cac068" fill-Rule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" font-size="none" textAnchor="none" ><g transform="scale(5.12,5.12)"><path d="M9.15625,6.3125l-2.84375,2.84375l15.84375,15.84375l-15.9375,15.96875l2.8125,2.8125l15.96875,-15.9375l15.9375,15.9375l2.84375,-2.84375l-15.9375,-15.9375l15.84375,-15.84375l-2.84375,-2.84375l-15.84375,15.84375z"></path></g></g>
